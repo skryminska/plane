8951f2578c348d436a96de7097379d8184339fde
skryminska <158287265+skryminska@users.noreply.github.com>
2024-01-31 13:51:54 +0100

    add code

--- game.js
+++ game.js
@@ -1,3 +1,30 @@
+class Main extends Phaser.Scene {
+
+    // This function essentially loads things into our game
+    preload() {
+        this.load.spritesheet('plane', 'assets/planesheet.png', { frameWidth: 98, frameHeight: 83 });
+        this.load.image('pipe', 'assets/pipe.png');
+        this.load.audio('jump', 'assets/jump.wav');
+    }
+
+    //  it runs once at the beginning of the game and
+    //  allows the user to place the things that they’ve preloaded with preload() and
+    //  create objects within our game such as animations, collision detectors, text, groups, and much more
+    create() {
+
+    }
+
+    // While preload() and create() run only once at the start of the game, update() runs constantly.
+    update() {
+        //Додаємо літак на сцену
+        this.plane = this.physics.add.sprite(0, 0, 'plane')
+        //Масштабуємо літак
+        this.plane.setScale(0.65, 0.65);
+        //Встановлюємо опорну точку літака
+        this.plane.setOrigin(0, 0.5);
+
+    }
+}
 const config = {
     type: Phaser.AUTO,
     width: 400,
@@ -7,7 +34,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 0}
+            gravity: { y: 0 }
         }
     }
 };
