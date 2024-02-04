ce6e2a6c15b1ee54d69a45d54cf31f1f046fbab9
skryminska <158287265+skryminska@users.noreply.github.com>
2024-02-05 00:56:52 +0200

    вставила сцену- не вийшло

--- game.js
+++ game.js
@@ -1,42 +1,41 @@
-class Main extends Phaser.Scene {
-
-    // This function essentially loads things into our game
-    preload() {
-        this.load.spritesheet('plane', 'assets/planesheet.png', { frameWidth: 98, frameHeight: 83 });
-        this.load.image('pipe', 'assets/pipe.png');
-        this.load.audio('jump', 'assets/jump.wav');
-    }
-
-    //  it runs once at the beginning of the game and
-    //  allows the user to place the things that they’ve preloaded with preload() and
-    //  create objects within our game such as animations, collision detectors, text, groups, and much more
-    create() {
-
-    }
-
-    // While preload() and create() run only once at the start of the game, update() runs constantly.
-    update() {
-        //Додаємо літак на сцену
-        this.plane = this.physics.add.sprite(0, 0, 'plane')
-        //Масштабуємо літак
-        this.plane.setScale(0.65, 0.65);
-        //Встановлюємо опорну точку літака
-        this.plane.setOrigin(0, 0.5);
-
-    }
-}
 const config = {
     type: Phaser.AUTO,
     width: 400,
     height: 490,
-    scene: Main, // Цю сцену ми створимо на 4-му кроці
-    backgroundColor: '#71c5cf',
+    scene:  class Main extends Phaser.Scene {
+
+        // This function essentially loads things into our game
+        preload() {
+            this.load.spritesheet('plane', 'assets/planesheet.png', {frameWidth: 98, frameHeight: 83});
+            this.load.image('pipe', 'assets/pipe.png');
+            this.load.audio('jump', 'assets/jump.wav');
+        }
+    
+        //  it runs once at the beginning of the game and
+        //  allows the user to place the things that they’ve preloaded with preload() and
+        //  create objects within our game such as animations, collision detectors, text, groups, and much more
+        create() {
+                    //Додаємо літак на сцену
+                this.plane = this.physics.add.sprite(0, 0, 'plane')
+                //Масштабуємо літак
+                this.plane.setScale(0.65, 0.65);
+                //Встановлюємо опорну точку літака
+                this.plane.setOrigin(0, 0.5);
+        }
+    
+        // While preload() and create() run only once at the start of the game, update() runs constantly.
+        update() {
+    
+        }
+    }
+
+    , backgroundColor: '#71c5cf', 
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 }
+            gravity: {y: 0}
         }
     }
+
 };
 
-const game = new Phaser.Game(config); 
